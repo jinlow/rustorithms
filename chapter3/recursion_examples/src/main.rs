a745bcaf7413c5099fc7292ff932556abe03c893
James Inlow <james.d.inlow@gmail.com>
2020-08-30 20:11:31 -0500

    Modify Recursion

--- chapter3/recursion_examples/src/main.rs
+++ chapter3/recursion_examples/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     print_rectangle(1);
     print_rectangle(4);
     print_rectangle(8);
+    let ar: [i32; 3] = [1, 2, 3];
+    println!("Recursive Sum of {:?} is: {}", ar, recursive_sum(&ar));
 }
 
 // The factorial function:
@@ -15,18 +17,34 @@ fn factorial(x: i32) -> i32 {
     }
 }
 
+// Right now every element in the array is being copied
+// so this would need to be made more efficient.
+fn recursive_sum<T: Copy + std::ops::Add<Output = T>>(x: &[T]) -> T {
+    if x.len() == 1 {
+        return x[0];
+    } else {
+        return x[0] + recursive_sum(&x[1..]);
+    }
+}
+
 fn rep_char(n: usize, cr: &str) -> String {
     std::iter::repeat(cr).take(n).collect::<String>()
 }
 
-fn print_rectangle(height: usize) {
-    for i in 0..height {
-        let x = if i == 0 { 0 } else { 1 * i };
+fn print_diags(level: usize, height: usize) {
+    if level == height {
+        return;
+    } else {
         println!(
             " {}/{}\\",
-            rep_char(height - (i + 1), " "),
-            rep_char((i + (i * 2)) - x, " ")
+            rep_char(height - (level + 1), " "),
+            rep_char(level * 2, " ")
         );
+        print_diags(level + 1, height);
     }
+}
+
+fn print_rectangle(height: usize) {
+    print_diags(0, height);
     println!(" {}", rep_char(height * 2, "-"))
 }
