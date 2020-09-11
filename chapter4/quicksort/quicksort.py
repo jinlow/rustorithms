b9f3e5b569bb342308fca976e21b4a8127685168
James Inlow <james.d.inlow@gmail.com>
2020-09-11 15:12:00 -0500

    Refining quicksort

--- chapter4/quicksort/quicksort.py
+++ chapter4/quicksort/quicksort.py
@@ -20,19 +20,46 @@ def pivot_list(x):
     return pivot
 
 
-def _quicksort(x):
+def _quicksort1(x):
     if len(x) < 2:
         return x
     else:
         pivot = pivot_list(x)
-        return _quicksort(x[:pivot]) + [x[pivot]] + _quicksort(x[(pivot + 1) :])
+        return _quicksort1(x[:pivot]) + [x[pivot]] + _quicksort1(x[(pivot + 1) :])
 
 
-def quicksort(x: List):
-    return _quicksort(x.copy())
+def quicksort1(x: List):
+    return _quicksort1(x.copy())
+
+
+def pivot_list_fl(x):
+    pivot = 0
+    low = 1
+    max_idx = high = len(x) - 1
+    while low < high:
+        while (x[high] >= x[pivot]) & (high > 0):
+            high -= 1
+        while (x[low] <= x[pivot]) & (low < max_idx):
+            low += 1
+        if low < high:
+            swap(x, high, low)
+    swap(x, pivot, high)
+    return high
+
+
+def _quicksort2(x):
+    if len(x) < 2:
+        return x
+    else:
+        pivot = pivot_list_fl(x)
+        return _quicksort2(x[:pivot]) + [x[pivot]] + _quicksort2(x[(pivot + 1) :])
+
+
+def quicksort2(x):
+    return _quicksort2(x.copy())
 
 
 a1 = [3, 1, 4, 5, -1, -6]
 print(f"The unsorted list: {a1}")
-print(f"The sorted list {quicksort(a1)}")
+print(f"The sorted list {quicksort1(a1)}")
 
--- chapter4/quicksort/src/main.rs
+++ chapter4/quicksort/src/main.rs
@@ -1,11 +1,53 @@
 fn main() {
     let mut a1: [i32; 8] = [3, 1, 4, 5, 10, -1, -6, 0];
-    println!("{:?}", a1);
+    println!("Unsorted array: {:?}", a1);
     quicksort(&mut a1);
-    println!("{:?}", a1);
-    let mut a2 = vec![4, 3, 2, 1];
-    quicksort(&mut a2);
-    println!("{:?}", a2);
+    println!("Sorted array: {:?}", a1);
+
+    // Second implementation
+    let mut a1: [i32; 8] = [3, 1, 4, 5, 10, -1, -6, 0];
+    println!("Unsorted array: {:?}", a1);
+    quicksort_mv(&mut a1);
+    println!("Sorted array: {:?}", a1);
+}
+
+// Pivot the array around the first element.
+fn pivot_array<T: std::cmp::Ord>(x: &mut [T]) -> usize {
+    let pivot: usize = 0;
+    let mut low: usize = 1;
+    let mut high: usize = x.len() - 1;
+    let max_idx = high;
+    while low < high {
+        while (x[high] >= x[pivot]) && (high > 0) {
+            high -= 1;
+        }
+        while (x[low] <= x[pivot]) && (low < max_idx) {
+            low += 1;
+        }
+        if low < high {
+            x.swap(high, low)
+        }
+    }
+    let pivot = if x[pivot] > x[high] {
+        x.swap(pivot, high);
+        high
+    } else {
+        pivot
+    };
+    pivot
+}
+
+// Quicksort implementation
+// This is the more efficient implementation unless the input array is already
+// sorted.
+fn quicksort<T: std::cmp::Ord>(x: &mut [T]) {
+    if x.len() < 2 {
+        return;
+    } else {
+        let pivot = pivot_array(x);
+        quicksort(&mut x[..pivot]);
+        quicksort(&mut x[(pivot + 1)..]);
+    }
 }
 
 // Move a given element in front of another given element
@@ -16,9 +58,9 @@ fn move_front<T>(x: &mut [T], i: usize, j: usize) {
 }
 
 // Pivot the array around the first element
-// Currently this uses the move infront function
+// Currently this uses the move front function
 // which is likely inefficient.
-fn pivot_array<T: std::cmp::Ord>(x: &mut [T]) -> usize {
+fn pivot_array_mv<T: std::cmp::Ord>(x: &mut [T]) -> usize {
     let mut pivot: usize = 0;
     for i in 1..(x.len()) {
         if x[i] < x[pivot] {
@@ -30,12 +72,49 @@ fn pivot_array<T: std::cmp::Ord>(x: &mut [T]) -> usize {
 }
 
 // Quicksort implementation
-fn quicksort<T: std::cmp::Ord>(x: &mut [T]) {
+fn quicksort_mv<T: std::cmp::Ord>(x: &mut [T]) {
     if x.len() < 2 {
         return;
     } else {
-        let pivot = pivot_array(x);
-        quicksort(&mut x[..pivot]);
-        quicksort(&mut x[(pivot + 1)..]);
+        let pivot = pivot_array_mv(x);
+        quicksort_mv(&mut x[..pivot]);
+        quicksort_mv(&mut x[(pivot + 1)..]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_array_of_i32() {
+        let mut a1: [i32; 8] = [3, 1, 4, 5, 10, -1, -6, 0];
+        let a2: [i32; 8] = [-6, -1, 0, 1, 3, 4, 5, 10];
+        quicksort(&mut a1);
+        assert_eq!(a1, a2)
+    }
+
+    #[test]
+    fn test_array_2_elements() {
+        let mut a1: [u8; 2] = [5, 4];
+        let a2: [u8; 2] = [4, 5];
+        quicksort(&mut a1);
+        assert_eq!(a1, a2);
+    }
+
+    #[test]
+    fn test_compare_quick_sorts() {
+        let mut a1: [i32; 8] = [3, 1, 4, 5, 10, -1, -6, 0];
+        let mut a2: [i32; 8] = [3, 1, 4, 5, 10, -1, -6, 0];
+        quicksort(&mut a1);
+        quicksort_mv(&mut a2);
+        assert_eq!(a1, a2);
+    }
+
+    #[test]
+    fn test_string_bytes() {
+        let s_sorted = String::from("  ASTbeeghhimmnoottu").into_bytes();
+        let mut s_unsorted = String::from("Seomthing About Them").into_bytes();
+        quicksort(&mut s_unsorted);
+        assert_eq!(s_sorted, s_unsorted);
     }
 }
