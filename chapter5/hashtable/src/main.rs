62c2f3f1b8b8fade78e9e2123fb78b8ac695920f
James Inlow <james.d.inlow@gmail.com>
2020-09-18 15:13:37 -0500

    Working on Hash Table

---
+++ chapter5/hashtable/src/main.rs
@@ -0,0 +1,20 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+fn main() {
+    let a = 10;
+    let b = "10";
+    let c = "10";
+    println!(
+        "Num Hash {}, str Hash {}, Other {}",
+        calc_hash(&a),
+        calc_hash(&b),
+        calc_hash(&c)
+    );
+}
+
+fn calc_hash<T: Hash>(x: &T) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    x.hash(&mut hasher);
+    hasher.finish()
+}
