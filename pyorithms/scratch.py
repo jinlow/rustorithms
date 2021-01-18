e782615ebd3080a773dd973d7d0eaa29356fc47c
jinlow <james.d.inlow@gmail.com>
2021-01-18 10:47:51 -0600

    Adding generic qsort

--- pyorithms/scratch.py
+++ pyorithms/scratch.py
@@ -1,11 +1,21 @@
 from pyorithms import quicksort as qs
 
 i = [4, 3, 5, 1]
-print(f"Int: Unsorted {i}, sorted {qs.qsort_i32(i)}")
+print(f"Int: Unsorted {i}, sorted {qs.qsort(i)}")
 
 s = ["4", "3", "5", "1"]
-print(f"Str: Unsorted {i}, sorted {qs.qsort_str(s)}")
+print(f"Str: Unsorted {i}, sorted {qs.qsort(s)}")
 
 f = [4.0, 3.0, 5.0, 1.0]
-print(f"Float: Unsorted {i}, sorted {qs.qsort_f32(f)}")
+print(f"Float: Unsorted {i}, sorted {qs.qsort(f)}")
 
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+    
+        greater = [i for i in array[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
--- pyorithms/src/pyquicksort.rs
+++ pyorithms/src/pyquicksort.rs
@@ -18,11 +18,43 @@ init_qsort_for_type!(qsort_i32, i32);
 init_qsort_for_type!(qsort_i64, i64);
 init_qsort_for_type!(qsort_str, &str);
 
+#[derive(FromPyObject)]
+pub enum QsInputs {
+    #[pyo3(transparent, annotation = "List[str]")]
+    Str(Vec<String>),
+    #[pyo3(transparent, annotation = "List[int]")]
+    Int(Vec<i64>),
+    #[pyo3(transparent, annotation = "List[float]")]
+    Flt(Vec<f64>),
+}
+
+/// Implementation of Quicksort in Rust
+#[pyfunction]
+pub fn qsort(x: QsInputs) -> PyResult<PyObject> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    match x {
+        QsInputs::Int(mut i) => {
+            rustorithms::quicksort(&mut i);
+            Ok(i.into_py(py))
+        }
+        QsInputs::Str(mut s) => {
+            rustorithms::quicksort(&mut s);
+            Ok(s.into_py(py))
+        }
+        QsInputs::Flt(mut f) => {
+            rustorithms::quicksort(&mut f);
+            Ok(f.into_py(py))
+        }
+    }
+}
+
 pub fn quicksort_init(m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(qsort_f32, m)?)?;
-    m.add_function(wrap_pyfunction!(qsort_f64, m)?)?;
-    m.add_function(wrap_pyfunction!(qsort_i32, m)?)?;
-    m.add_function(wrap_pyfunction!(qsort_i64, m)?)?;
-    m.add_function(wrap_pyfunction!(qsort_str, m)?)?;
+    m.add_function(wrap_pyfunction!(qsort, m)?)?;
+    // m.add_function(wrap_pyfunction!(qsort_f32, m)?)?;
+    // m.add_function(wrap_pyfunction!(qsort_f64, m)?)?;
+    // m.add_function(wrap_pyfunction!(qsort_i32, m)?)?;
+    // m.add_function(wrap_pyfunction!(qsort_i64, m)?)?;
+    // m.add_function(wrap_pyfunction!(qsort_str, m)?)?;
     Ok(())
 }
